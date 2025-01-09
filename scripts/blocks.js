72d8d299ed221c50dea3b147b36efc944fd26f12
duydz <phuongduy.via@gmail.com>
2025-01-09 17:14:18 +0700

    first commit

---
+++ scripts/blocks.js
@@ -0,0 +1,21 @@
+export const blocks = {
+  empty: {
+    id: 0,
+    name: "empty",
+  },
+  grass: {
+    id: 1,
+    name: "grass",
+    color: 0x4caf50,
+  },
+  dirt: {
+    id: 2,
+    name: "dirt",
+    color: 0x8b4513,
+  },
+  stone: {
+    id: 3,
+    name: "stone",
+    color: 0x808080,
+  }
+};
---
+++ scripts/main.js
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Stats from 'three/examples/jsm/libs/stats.module.js'
+import { World } from "./world";
+import { createUI } from "./ui";
+
+const stats = new Stats();
+document.body.append(stats.dom);
+
+//Renderer setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x80a0e0)
+document.body.appendChild(renderer.domElement);
+
+//Camera setup
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+camera.position.set(-32, 16, -32);
+camera.lookAt(0, 0, 0);
+
+//Control setup
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(16, 0, 16);
+controls.update();
+
+
+//Scene setup
+const scene = new THREE.Scene();
+const world = new World();
+world.generate();
+scene.add(world);
+
+//Light
+function setupLights() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, 1, -0.5);
+  scene.add(light2);
+
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
+}
+
+
+//Render loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  stats.update();
+}
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+setupLights();
+animate();
+createUI(world);
